0ec0cb63270e22a9673a7f5987a11fc9e7af291a
tgendrich <gendricht@gmail.com>
2021-08-17 18:23:58 -0500

    ignorefilecreated

---
+++ BestbuyORM/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BestbuyORM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
